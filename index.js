86693500895992c4b70a8b43e057e2c7b0cf8756
nandan-ops <nandanramdani608@gmail.com>
2023-10-25 21:39:19 +0700

    update

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-// const client = new Client({
-//   puppeteer: {
-//     headless: true,
-//     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-//   },
-// });
-
-const client = 0;
+const client = new Client();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,51 +21,51 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-// client.initialize();
+client.initialize();
 
-// var today = new Date();
-// var now = today.toLocaleString();
+var today = new Date();
+var now = today.toLocaleString();
 
-// io.on("connection", (socket) => {
-//   socket.emit("message", `${now} Connected`);
+io.on("connection", (socket) => {
+  socket.emit("message", `${now} Connected`);
 
-//   client.on("qr", (qr) => {
-//     qrcode.toDataURL(qr, (err, url) => {
-//       socket.emit("qr", url);
-//       socket.emit("message", `${now} QR Code received`);
-//     });
-//   });
+  client.on("qr", (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit("qr", url);
+      socket.emit("message", `${now} QR Code received`);
+    });
+  });
 
-//   client.on("ready", () => {
-//     socket.emit("message", `${now} WhatsApp is ready!`);
-//   });
-// });
+  client.on("ready", () => {
+    socket.emit("message", `${now} WhatsApp is ready!`);
+  });
+});
 
-// app.post("/send", async (req, res) => {
-//   const phone = req.body.phone;
-//   const message = req.body.message;
+app.post("/send", async (req, res) => {
+  const phone = req.body.phone;
+  const message = req.body.message;
 
-//   client
-//     .sendMessage(`${phone}@c.us`, message)
-//     .then((response) => {
-//       res.status(200).json({
-//         error: false,
-//         data: {
-//           message: "Pesan terkirim",
-//           meta: response,
-//         },
-//       });
-//     })
-//     .catch((error) => {
-//       res.status(200).json({
-//         error: true,
-//         data: {
-//           message: "Error send message",
-//           meta: error,
-//         },
-//       });
-//     });
-// });
+  client
+    .sendMessage(`${phone}@c.us`, message)
+    .then((response) => {
+      res.status(200).json({
+        error: false,
+        data: {
+          message: "Pesan terkirim",
+          meta: response,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(200).json({
+        error: true,
+        data: {
+          message: "Error send message",
+          meta: error,
+        },
+      });
+    });
+});
 
 server.listen(PORT, () => {
   console.log("App listen on port ", PORT);
